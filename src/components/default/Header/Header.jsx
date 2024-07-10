940dee30565889df903cfb428ebcbf73ff4550fd
FeHunter <hunterfe1111@gmail.com>
2024-07-10 19:58:56 -0300

    Iniciando criação do componente Header

---
+++ src/components/default/Header/Header.jsx
@@ -0,0 +1,11 @@
+import style from './Header.module.css';
+
+export function Header () {
+    return (
+        <header>
+            <div>
+                <h1>Felipe Rodrigues <span>Hunter</span></h1>
+            </div>
+        </header>
+    );
+}
