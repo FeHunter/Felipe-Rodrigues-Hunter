ffcafbf04c991791a1d02a24fa37a133dd090a19
FeHunter <hunterfe1111@gmail.com>
2025-08-01 20:38:06 -0300

    fix: ajustando layout.

--- src/components/Content/PopUpWindow/PopUpWindow.jsx
+++ src/components/Content/PopUpWindow/PopUpWindow.jsx
@@ -7,11 +7,21 @@ export function PopUpWindow ({label, visible, closeClick, children}) {
             {visible &&
             <>
                 <div  className={style.window}>
-                    <div className={style.header}>
-                        <p className={style.label}><i class="fa-solid fa-terminal"></i> {label}</p>
-                        <span className={style.closeButton} onClick={closeClick}><i class="fa-regular fa-circle-xmark"></i></span>
+                    <div className={style.content}>
+                        <div className={style.windowContent}>
+                            <div className={style.header}>
+                                <p className={style.label}><i class="fa-solid fa-terminal"></i> {label}</p>
+                                <span className={style.closeButton} onClick={closeClick}><i class="fa-regular fa-circle-xmark"></i></span>
+                            </div>
+                            <div className={style.windowLocal}> {children} </div>
+                        </div>
                     </div>
-                    <div className={style.content}> {children} </div>
+                    <div className={style.bottom}>
+                        <button className={style.bottomBtn}> <i class="fa-solid fa-file-lines"></i> </button>
+                        <button className={style.bottomBtn}> <i class="fa-solid fa-images"></i> </button>
+                        <button className={style.bottomBtn}> <i class="fa-solid fa-photo-film"></i> </button>
+                        <button className={style.bottomBtn}> <i class="fa-solid fa-link"></i> </button>
+                    </div>
                 </div>
             </>}
         </section>
