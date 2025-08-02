78cf3cd5b343f4b70c0f0b865637c3fea9fd8084
FeHunter <hunterfe1111@gmail.com>
2025-08-02 13:33:01 -0300

    feat: opção de minimizar e maximizar janela de projetos.

--- src/components/Content/PopUpWindow/PopUpWindow.jsx
+++ src/components/Content/PopUpWindow/PopUpWindow.jsx
@@ -1,17 +1,35 @@
+import { useEffect, useState } from 'react';
 import style from './PopUpWindow.module.css';
 
 export function PopUpWindow ({label, visible, closeClick, children}) {
 
+    // max and min window width
+    const windowMin = { width: '75%', height: '80%', transition: '.5s' }
+    const windowMax = { width: '100%', height: '100%', transition: '.5s' }
+    const [windowFull, setWindowFull] = useState(false)
+    const [windowSize, setWindowSize] = useState(windowMin)
+    const changeWindowSize = () => {
+        setWindowFull((prev) => !prev )
+    }
+    useEffect(()=>{
+        setWindowSize(windowFull ? windowMax : windowMin)
+        console.log(windowFull)
+    },[windowFull])
+
     return (
-        <section className={`${visible && style.container} ${visible && style.animationOn || style.animationOff}`}>
-            {visible &&
-            <>
-                <div  className={style.window}>
+        <>
+            <section className={`${visible && style.container} ${visible && style.animationOn || style.animationOff}`}>
+                {visible && <> <div  className={style.mainWindow}>
                     <div className={style.content}>
-                        <div className={style.windowContent}>
+                        <div className={style.windowContent} style={windowSize}>
                             <div className={style.header}>
                                 <p className={style.label}><i class="fa-solid fa-terminal"></i> {label}</p>
-                                <span className={style.closeButton} onClick={closeClick}><i class="fa-regular fa-circle-xmark"></i></span>
+                                <div className={style.headerButtons}>
+                                    <span className={style.closeButton} onClick={changeWindowSize}>
+                                        {windowFull && <i class="fa-solid fa-compress"></i> || <i class="fa-solid fa-expand"></i>}
+                                    </span>
+                                    <span className={style.closeButton} onClick={closeClick}> <i class="fa-regular fa-circle-xmark"></i> </span>
+                                </div>
                             </div>
                             <div className={style.windowLocal}> {children} </div>
                         </div>
@@ -22,8 +40,8 @@ export function PopUpWindow ({label, visible, closeClick, children}) {
                         <button className={style.bottomBtn}> <i class="fa-solid fa-photo-film"></i> </button>
                         <button className={style.bottomBtn}> <i class="fa-solid fa-link"></i> </button>
                     </div>
-                </div>
-            </>}
-        </section>
+                </div> </>}
+            </section>
+        </>
     )
 }
