6f71a07a7835be99d4b71b0adf19f973f25aa3d1
FeHunter <hunterfe1111@gmail.com>
2025-09-26 20:12:03 -0300

    fix: ajuste para salvar dados no banco de dados, supabase.

--- src/pages/add_blog_post/add_blog_post.jsx
+++ src/pages/add_blog_post/add_blog_post.jsx
@@ -3,11 +3,19 @@ import style from './add_blog_post.module.css'
 import * as yup from 'yup'
 import { useEffect, useState } from 'react'
 
+// supabase
+import { supabase } from '../../services/supabase/supabaseClient'
+
 export function AddBlogPostForm () {
 
     const validate = yup.object().shape({
-        title: yup.string().required("O Título não pode esta vazio.")
+        title: yup.string().required("O Título não pode esta vazio."),
+        about: yup.string().required("Descrição do post é necessária."),
+        text_content: yup.string().required("Escreva sobre o projeto."),
     })
+
+    // Images
+    const [images, setImages] = useState(null)
 
     // Add links
     const [inputVieoLink, setInputVieoLink] = useState('')
@@ -30,40 +38,72 @@ export function AddBlogPostForm () {
         setVideoLinks(upt)
     }
 
+    // Save Post - Data base
+    const AddPostToDataBase = async (values) => {
+        // add post texts
+        const post_to_upload = { 'title': values.title, 'text_content': values.text_content, 'about': values.about, 'created_at': values.created_at }
+        const { data: post, error: post_error } = await supabase.from('Blog_Post').insert([post_to_upload]).select().single()
+        if (post_error) console.log(post_error)
+        if (post) {
+            console.log(post)
+            // add images links
+            if (images){
+                for (const img of images){
+                    const to_upload = { 'post_id': post.id, 'img_link': img }
+                    const {error: img_link_error} = await supabase.from('Blog_Images').insert([to_upload])
+                    if (img_link_error) console.log(`Erro ao salvar image: ${img}, mais detalhes: ${img_link_error.message}`)
+                }
+            }
+            // add video links
+            if (videoLinks.length > 0){
+                for (const link of videoLinks){
+                    const to_upload = { 'post_id': post.id, 'video_link': link }
+                    const {error: video_link_error} = await supabase.from('Blog_video_links').insert([to_upload])
+                    if (video_link_error) console.log(`Erro ao salvar link: ${link}, mais detalhes: ${video_link_error.message}`)
+                }
+            }
+        }
+    }
+
     return (
         <main className={style.container}>
             <h2>Adicionar um post ao blog</h2>
             <Formik
-                initialValues={{ title: '', about: '', textContent: '', images: null, videoLink: null, createdAt: null }}
+                initialValues={{ title: '', about: '', text_content: '', images: null, video_link: null, created_at: null }}
                 validationSchema={validate}
-                onSubmit={()=>{
-                    createdAt = Date.now()
+                onSubmit={(values)=>{
+                    values.created_at = Date.now()
+                    AddPostToDataBase(values)
                 }}
             >
                 <Form className={style.formContainer}>
                     <div className={style.field}>
                         <label htmlFor="Title">Título do Post</label>
                         <Field className={style.inputField} name="title" id="title" type="text" placeholder="Projeto X" />
-                        <ErrorMessage name='title' />
+                        <ErrorMessage name='title' component="p" className={style.errorMessage} />
                     </div>
                     <div className={style.field}>
                         <label htmlFor="about">Descrição do Post, breve resumo sobre o post.</label>
                         <Field className={style.inputField} name="about" id="about" type="text" placeholder="Este projeto é sobre..." />
-                        <ErrorMessage name='about' />
+                        <ErrorMessage name='about' component="p" className={style.errorMessage} />
                     </div>
                     <div className={style.field}>
-                        <label htmlFor="textContent">Conteúdo do post, todo o texto.</label>
-                        <Field className={style.inputField} name="textContent" id="textContent" type="text" as="textarea" rows="10" placeholder="Este projeto é sobre..." />
-                        <ErrorMessage name='textContent' />
+                        <label htmlFor="text_content">Conteúdo do post, todo o texto.</label>
+                        <Field className={style.inputField} name="text_content" id="text_content" type="text" as="textarea" rows="10" placeholder="Este projeto é sobre..." />
+                        <ErrorMessage name='text_content' component="p" className={style.errorMessage} />
                     </div>
                     <div className={style.field}>
                         <label htmlFor="imagesLinks">Imagens do projeto</label>
-                        <Field className={style.inputField} name="imagesLinks" id="imagesLinks" type="file" multiple="multiple" />
-                        <ErrorMessage name='imagesLinks' />
+                        <Field className={style.inputField} name="imagesLinks" id="imagesLinks" type="file" multiple="multiple"
+                            value={images} onChange={(e)=>{
+                                const allFiles = Array.from(e.target.files)
+                                setImages(allFiles)
+                            }}/>
+                        <ErrorMessage name='imagesLinks' component="p" className={style.errorMessage} />
                     </div>
                     <div className={style.field}>
-                        <label htmlFor="VídeoLink">Imagens do projeto</label>
-                        <Field className={style.inputField} name="VídeoLink" id="VídeoLink" type="text" placeholder="https://"
+                        <label htmlFor="video_link">Imagens do projeto</label>
+                        <Field className={style.inputField} name="video_link" id="video_link" type="text" placeholder="https://"
                             value={inputVieoLink} onChange={(e)=>{setInputVieoLink(e.target.value)}} />
                         <nav className={style.videoLinksContainer}>
                             {videoLinks.length > 0 && videoLinks.map((link, i) => {
@@ -76,7 +116,7 @@ export function AddBlogPostForm () {
                             })}
                         </nav>
                         <button type='button' onClick={()=>{AddVideoLink()}}>Adicionar Link</button>
-                        <ErrorMessage name='VídeoLink' />
+                        <ErrorMessage name='video_link' />
                     </div>
                     <div className={style.field}>
                         <button type='submit' className={style.addPostBtn}>Salvar Post</button>
